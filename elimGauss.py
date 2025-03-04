e8bacae67d15ba6ecf9585700faefbee457b5e2c
Jan Matas Cantos <96077201+Jan1011000110@users.noreply.github.com>
2025-03-04 20:50:48 +0100

    Update elimGauss.py

--- elimGauss.py
+++ elimGauss.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 def elimGauss(A,b,tol=1.e-10):
+    n = b.shape[0]
+    if U.shape[0] != U.shape[1]:
+        raise ValueError(f"La matriu és {U.shape[0])}x{U.shape[1]} i ha de ser quadrada!")
+    if n != U.shape[0]:
+        raise ValueError(f"Dimensions incompatibles! (files matriu) {U.shape[0]} != {n} (elements vector)")
+    for i in range(n):
+        if np.abs(U[i,i]) < tol:
+            raise ValueError("Element diagonal massa petit!")
   for k in range(0,n-1):
     for i in range(k+1,n):
       m = A[i,k]/A[k,k]
