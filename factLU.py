661d9adce481e0192f1d45e46db17521e06de4ee
Jan Matas Cantos <96077201+Jan1011000110@users.noreply.github.com>
2025-03-05 11:22:13 +0100

    Update factLU.py

--- factLU.py
+++ factLU.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def factLU(A, tol = 1.e-10):
-    M =
+    M = A.copy()
     n = A.shape[0]
     if n != A.shape[1]:
         raise ValueError("La matriu no és quadrada!")
@@ -9,11 +9,10 @@ def factLU(A, tol = 1.e-10):
         if np.abs(A[i,i]) < tol:
             raise ValueError("Pivot massa petit! Matriu (pròxima a) singular!")
             
-    for k in range(0,n-1):
+    for k in range(n-1):
         for i in range(k+1,n):
             m = A[i,k]/A[k,k]
-            b[i] -= m*b[k]
-
-            A[i,k] = 0
+            A[i,k] = m
             for j in range(k+1,n):
                 A[i,j] -= m*A[k,j]
+    return M
