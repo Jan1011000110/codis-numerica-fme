96c3e79f6ad49045edf791164dc8c05e69e9f322
Jan Matas Cantos <96077201+Jan1011000110@users.noreply.github.com>
2025-03-06 20:12:09 +0100

    Create LUpivesg.py

---
+++ LUpivesg.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def LUpivesg(A, tol = 1.e-10):
+    M = A.copy()
+    n = A.shape[0]
+    p = np.array([i for i in range(n)])
+    if n != A.shape[1]:
+        raise ValueError("La matriu no és quadrada!")
+    
+    for k in range(0,n-1):
+        s = np.array([np.max(np.abs(M[i,k:])) for i in range(k,n)])
+        index = k + np.argmax(np.abs(M[k:,k]/s)) 
+        p[[k,index]] = p[[index,k]]
+        M[[k,index]] = M[[index,k]]
+        for i in range(k+1,n):
+            if np.abs(M[k,k]) < tol:
+                raise ValueError("Pivot massa petit! Matriu (pròxima a) singular!")
+            m = M[i,k]/M[k,k]
+            M[i,k] = m
+            for j in range(k+1,n):
+                M[i,j] -= m*M[k,j]
+    return M, p
