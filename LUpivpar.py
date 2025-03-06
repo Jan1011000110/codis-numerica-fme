5aa4840c27a65beff4da0d51edc5cc02861ffb4e
Jan Matas Cantos <96077201+Jan1011000110@users.noreply.github.com>
2025-03-06 19:42:48 +0100

    Create LUpivpar.py

---
+++ LUpivpar.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def LUpivpar(A, tol = 1.e-10):
+    M = A.copy()
+    n = A.shape[0]
+    p = np.array([i for i in range(n)])
+    if n != A.shape[1]:
+        raise ValueError("La matriu no és quadrada!")
+    
+    for k in range(0,n-1):
+        index = k + np.argmax(np.abs(M[k:,k])) 
+        p[[k,index]] = p[[index,k]]
+        M[[k,index]] = M[[index,k]]
+        for i in range(k+1,n):
+            if np.abs(M[k,k]) < tol:
+                raise ValueError("Pivot massa petit! Matriu (pròxima a) singular!")
+            m = M[i,k]/M[k,k]
+            M[i,k] = m
+            for j in range(k+1,n):
+                M[i,j] -= m*M[k,j]
+    return M, p
