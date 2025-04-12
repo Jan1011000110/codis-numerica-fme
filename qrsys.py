b01b2fb0a4f98d8f2c7a399ff0cc65f971e8190f
Jan Matas Cantos <96077201+Jan1011000110@users.noreply.github.com>
2025-04-12 12:19:49 +0200

    Update qrsys.py

--- qrsys.py
+++ qrsys.py
@@ -3,7 +3,7 @@ def qrsys(A,b):
     R = A.copy()
     Qtb = b.copy()
     for k in range(n):
-        u = np.array(A[k:,k]).reshape(-1, 1)
+        u = np.array(R[k:,k]).reshape(-1, 1)
         u[0] = u[0] + np.sign(u[0])*np.linalg.norm(u)
         u = u/np.linalg.norm(u)
         R[k:,k:] = R[k:,k:] - u @ (2 * u.T @ R[k:,k:])
