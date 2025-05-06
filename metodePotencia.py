57cb1f9259cd4b6fbd7af888b976c46ccaf2a13c
Jan Matas Cantos <96077201+Jan1011000110@users.noreply.github.com>
2025-05-06 16:43:58 +0200

    Update metodePotencia.py

--- metodePotencia.py
+++ metodePotencia.py
@@ -1,9 +1,9 @@
 def metode_potencia(A, z, tol=1.e-10, maxIter=100):
-    sigma = z.T@A@z
+    sigma = np.conjugate(z)@A@z
     for i in range(1, maxIter+1):
         prod_Az = A@z
         nz = prod_Az/np.linalg.norm(prod_Az)
-        nsigma = nz.T@A@nz
+        nsigma = np.conjugate(nz)@A@nz
         dif = np.abs(nsigma - sigma)
         z, sigma = nz, nsigma
         if dif < tol:
